52058cf90d4a60549a3c41084a160512e57f2975
tazz <tasman.vose@gmail.com>
2026-08-25 11:36:55 -0400

    refactor: 🔨 even more refactors add TODOs

--- libs/controllers/all/include/hypha/controllers.h
+++ libs/controllers/all/include/hypha/controllers.h
@@ -1,15 +1,23 @@
 #ifndef HYPHA_CONTROLLERS_H
 #define HYPHA_CONTROLLERS_H
 
-#include "hypha/archive_controller.h"
 #include "hypha/controller.h"
 #include "hypha/directory_controller.h"
 #include "hypha/package_controller.h"
 #include "hypha/package_manager_controller.h"
-#include "hypha/repository_controller.h"
 #include "hypha/symlink_controller.h"
 #include "hypha/template_controller.h"
 #include "hypha/test_controller.h"
+
+#ifdef HYPHA_HAS_ARCHIVE
+// TODO(@s0cks): need to change FOR_EACH_CONTROLLER
+#include "hypha/archive_controller.h"
+#endif  // HYPHA_HAS_ARCHIVE
+
+#ifdef HYPHA_HAS_GIT2
+// TODO(@s0cks): need to change FOR_EACH_CONTROLLER
+#include "hypha/repository_controller.h"
+#endif  // HYPHA_HAS_GIT2
 
 #define FOR_EACH_CONTROLLER(V) \
   V(Controller)                \
