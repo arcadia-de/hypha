5cf59fa44b1d3588a333eb74f0d4562705d3f6c0
tazz <tasman.vose@gmail.com>
2026-08-27 10:30:42 -0400

    refactor: 🔨 cleanup the test controller and spec

--- libs/controllers/test/src/test_controller.c
+++ libs/controllers/test/src/test_controller.c
@@ -7,35 +7,53 @@
 
 #include "hypha.h"
 #include "hypha/log.h"
+#include "hypha/planned_action.h"
 #include "hypha/planner.h"
 #include "hypha/validation_log.h"
 #include "hypha/validation_result.h"
+
+static const uint32_t kDefaultSleepSeconds = 1;
+
+typedef struct {
+  uint32_t sleep;
+} TestSpec;
+
+static const char kSleepField[] = "sleep";
+static inline void GetSleepField(const Resource* res, uint32_t* result) {
+  ASSERT(res);
+  ASSERT(res->spec.doc);
+  json_t* sleep = json_object_get(res->spec.doc, kSleepField);
+  if (!sleep || !json_is_integer(sleep)) {
+    (*result) = kDefaultSleepSeconds;
+    return;
+  }
+
+  (*result) = (uint32_t)json_integer_value(sleep);
+}
+
+thread_local TestSpec test_spec;
 
 DEFINE_CONTROLLER_OBSERVE_FN(Test) {
   return kStatusOk;
 }
 
 DEFINE_CONTROLLER_VALIDATE_FN(Test) {
-  ValidationResult* vr = NewValidationResult(vlog);
+  ValidationResult* vr = NewWarningValidationResult(vlog, desired, "This message always exists for Test resources");
   ASSERT(vr);
-  vr->kind = kValidationWarning;
-  vr->resource = desired;
-  snprintf(vr->reason, HYPHA_REASON_MAX_LENGTH, "This message always exists for Test resources");
   return true;
 }
 
 DEFINE_CONTROLLER_PLAN_FN(Test) {
   ASSERT(desired);
-  PlannedAction* new_action = NewPlannedAction(pl);
-  new_action->resource = desired;
-  new_action->action = kCreateAction;
-  clock_gettime(CLOCK_REALTIME, &new_action->timestamp);
-  snprintf(new_action->reason, HYPHA_REASON_MAX_LENGTH, "Test resources are always created");
+  GetSleepField(desired, &test_spec.sleep);
+  PlannedAction* action = NewCreatePlannedAction(pl, desired, "Test resources are always created");
+  ASSERT(action);
   return kCreateAction;
 }
 
 DEFINE_CONTROLLER_APPLY_FN(Test) {
-  sleep(2);
+  DLOG_INFO("sleeping for %d seconds", test_spec.sleep);
+  sleep(test_spec.sleep);
   return kStatusOk;
 }
 
